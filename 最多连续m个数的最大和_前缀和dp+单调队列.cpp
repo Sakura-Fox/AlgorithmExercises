da1d6a547cc05fa16855c26de1c853f9deb2e5f9
Sakura-Fox <2015037952@qq.com>
2021-03-24 02:58:44 +0800

    增加了题目、思路、tips

--- 最多连续m个数的最大和_前缀和dp+单调队列.cpp
+++ 最多连续m个数的最大和_前缀和dp+单调队列.cpp
@@ -1,3 +1,12 @@
+/*
+    题面：n(1e5)个数，每个数[-100,100]，取数必须连续且不超过m，问取数最大的和，要求nlogn
+    来源：同学字节线上笔试
+    思路：一开始想模仿LIS，用dp保持以i为末尾的结果，但是有m这个限制，实现不了。
+    正确的解法：1. 求以i结尾的部分和其实等价于sum[i] - sum[head]
+                2. 于是问题变成求min(sum[head])
+                3. 长度为m的滑动窗口一般用单调队列来解决
+    Tips：队列中只存下标，可以提升效率
+*/
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
