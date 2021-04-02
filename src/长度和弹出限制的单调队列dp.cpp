e1744270821c01d83c0474b51beb43a60c08f19f
Sakura-Fox <2015037952@qq.com>
2021-04-02 17:59:32 +0800

    单调队列dp，stl被卡改用数组实现

--- src/长度和弹出限制的单调队列dp.cpp
+++ src/长度和弹出限制的单调队列dp.cpp
@@ -37,10 +37,15 @@
                 dp[i][j+1] = dp[i-1][lst.front()] + j*a[i];
                 j++;
             }
+
+            还是错误，第一是必须j*(ll)a[i]，否则会溢出
+            修正后大部分ac，少量tle，被卡了100ms
+
+            猜想stl的list效率问题，改用数组实现
+            list 0.6s tle
+            数组 0.1s ac
 */
 
-
-//写了4h没过，有待debug
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -48,6 +53,7 @@ const int maxn = 5e3 + 5;
 int a[maxn];
 //遍历到第i个点,当前队列j个数（包含当前），累加的答案
 ll dp[maxn][maxn];
+int q[maxn];
 // dp[i][j] = max{dp[i-1][k] + j*a[i]}
 // j == k - t + 1; 1 <= j,k <= w; 0 <= t <= s
 // k == j+t-1  max(1,j-1)<= k <= min(j+s-1,w)
@@ -62,27 +68,49 @@ signed main()
     dp[0][0] = 0;
     for (int i = 1; i <= n; i++)
     {
-        list<int> lst;
-        lst.push_back(0); //存dp[i-1][j]的j
+        // list<int> lst;
+        // lst.push_back(0); //存dp[i-1][j]的j
+        // int j = 1;
+        // for (int k = 1; k <= w; k++) //如何保证 k<=j+s-1? k==s j==1 k++ j++ k-j==s-1
+        // {
+        //     while (!lst.empty() && dp[i - 1][lst.back()] <= dp[i - 1][k])
+        //         lst.pop_back();
+        //     lst.push_back(k);
+        //     while (!lst.empty() && j - lst.front() > 1)
+        //         lst.pop_front();
+        //     if (k >= s)
+        //     {
+        //         dp[i][j] = dp[i - 1][lst.front()] + j * (ll)a[i];
+        //         j++;
+        //     } 
+        // }
+        // while (j <= w) //k不变 j++ k-j<=s-1
+        // {
+        //     while (!lst.empty() && j - lst.front() > 1)
+        //         lst.pop_front();
+        //     dp[i][j] = dp[i - 1][lst.front()] + j *(ll) a[i];
+        //     j++;
+        // }
+        int l = 1, r = 1;
+        q[l] = 0;
         int j = 1;
-        for (int k = 1; k <= w; k++)
+        for(int k=1;k<=w;k++)
         {
-            while (!lst.empty() && dp[i - 1][lst.back()] <= dp[i - 1][k])
-                lst.pop_back();
-            lst.push_back(k);
-            while (!lst.empty() && j - lst.front() > 1)
-                lst.pop_front();
-            if (k >= s)
-            {
-                dp[i][j] = dp[i - 1][lst.front()] + j * a[i];
+            while(l<=r&&dp[i-1][q[r]]<=dp[i-1][k])
+                r--;
+            q[++r] = k;
+            while(l<=r&&j-q[l]>1)
+                l++;
+            if(k>=s){
+                dp[i][j] = dp[i-1][q[l]] + j *(ll) a[i];
                 j++;
-            } 
+            }
         }
-        while (j <= w)
+        while(j<=w)
         {
-            while (!lst.empty() && j - lst.front() > 1)
-                lst.pop_front();
-            dp[i][j] = dp[i - 1][lst.front()] + j * a[i];
+            while(l<=r&&j-q[l]>1)
+                l++;
+            dp[i][j] = dp[i-1][q[l]] + j *(ll) a[i];
             j++;
         }
     }
